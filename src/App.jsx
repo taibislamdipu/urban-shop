b2d67d6fb24d34825e2ee4a461c85b988e3e811d
taibislamdipu <mailtaibislam@gmail.com>
2025-06-24 17:50:41 +0600

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
-import CartDetails from "./components/cart/CartDetails";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Newsletter from "./components/Newsletter";
-import ProductList from "./components/product/ProductList";
-import TopPromoBanner from "./components/TopPromoBanner";
+import { ToastContainer } from "react-toastify";
 import { ProductContext } from "./context";
 import { getAllProducts } from "./data/products";
+import Page from "./Page";
 
 function App() {
   const [cartData, setCartData] = useState([]);
@@ -24,18 +20,8 @@ function App() {
         setProducts,
       }}
     >
-      <TopPromoBanner />
-      <Header />
-
-      <main className="container mx-auto px-4 md:px-8 py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <ProductList />
-          <CartDetails />
-        </div>
-      </main>
-
-      <Newsletter />
-      <Footer />
+      <Page />
+      <ToastContainer />
     </ProductContext.Provider>
   );
 }
---
+++ src/Page.jsx
@@ -0,0 +1,25 @@
+import CartDetails from "./components/cart/CartDetails";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Newsletter from "./components/Newsletter";
+import ProductList from "./components/product/ProductList";
+import TopPromoBanner from "./components/TopPromoBanner";
+
+export default function Page() {
+  return (
+    <>
+      <TopPromoBanner />
+      <Header />
+
+      <main className="container mx-auto px-4 md:px-8 py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <ProductList />
+          <CartDetails />
+        </div>
+      </main>
+
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
