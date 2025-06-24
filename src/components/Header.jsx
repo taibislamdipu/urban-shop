77feb6a1f1e65d5ca1a44a243fb05766b3720c88
taibislamdipu <mailtaibislam@gmail.com>
2025-06-24 17:13:19 +0600

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+import { FaRegUser } from "react-icons/fa6";
+import { IoCartOutline, IoSearchOutline } from "react-icons/io5";
+
 export default function Header() {
   return (
     <header className="border-b border-gray-200 py-4 px-4 md:px-8">
@@ -22,62 +25,23 @@ export default function Header() {
         </nav>
 
         <div className="flex items-center space-x-4">
-          <div className="relative hidden md:block w-64">
+          <div className="relative hidden md:block w-64 border rounded-full border-gray-300">
             <input
               type="text"
               placeholder="Search for products..."
               className="w-full bg-gray-100 rounded-full py-2 px-4 text-sm"
             />
             <span className="absolute right-3 top-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
+              <IoSearchOutline />
             </span>
           </div>
 
           <a href="#" className="hover:text-gray-500 transition-colors">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-              />
-            </svg>
+            <IoCartOutline size={30} />
           </a>
 
           <a href="#" className="hover:text-gray-500 transition-colors">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
+            <FaRegUser size={23} />
           </a>
         </div>
       </div>
